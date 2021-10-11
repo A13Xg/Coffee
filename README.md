a3a2e36506e2c342c994dcd4adeec9a9983d648f
Alexander Goodman <38967711+A13Xg@users.noreply.github.com>
2021-10-11 10:03:25 -0700

    Init Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Coffee
-Keep your computer awake without installing software
+![Coffee!](https://cdn.iconscout.com/icon/free/png-256/hot-drinks-1533452-1299575.png)
+
+#
+
+## Keep your computer awake without installing software
+
+#### Coffee is a powershell application that was complied into a Windows Binary (EXE) for your convenience. Note that the 'coffee-gui.ps1' and 'Coffee.exe' will operate the same- feel free to read through the source code and run the .ps1 script instead of the EXE if you dont trust blindly running executables.
+
+#
+
+> Note some Anti-Virus programs will flag this executable because it isn't signed, etc. If you don't trust it then please use the raw script instead. [VirusTotal Link](https://www.virustotal.com/gui/file/d570f91f7eb55d49a60c20b3299bcbca95b846bbbd875ebc3dc7dee5674362f1?nocache=1)
+
+# Installation
+## You can download the releases here:
+>[All Releases](https://www.example.com)
+>
+>### Version 1.2 [v1.2]()
+> ` MD5 HASH:   `
---
+++ coffee-gui.ps1
@@ -0,0 +1,101 @@
+<#
+Code written by A13Xg
+Property of Alexander Goodman
+A13Xg Industries
+#>
+
+## ABOUT
+# This code prevents a system from going to sleep due to a forced Group Policy or other machine settings.
+# This is accomplished by toggling the 'Scroll Lock' key on and off every 45 seconds.
+# >NOTE< This application is NOT intended to circumvent security measures. When running this application ensure the system is physically supervised and protected.
+
+#region \/ DEFINE ASSETS \/
+    #// Function to get current directory and specify path to Assets
+    $Assets = [PSCustomObject]@{
+        ImgFull     = Join-Path -Path (Get-Location) -ChildPath "\Assets\coffee-full.png"
+        ImgEmpty    = Join-Path -Path (Get-Location) -ChildPath "\Assets\coffee-empty.png"
+        Icon        = Join-Path -Path (Get-Location) -ChildPath "\Assets\coffee.ico"
+}
+#endregion /\ DEFINE ASSETS /\
+
+#region \/ FUNCTIONS \/
+    #// Function to change the image in the UI to be full
+function StatusBang {
+        $image.imageLocation = $Assets.ImgFull
+    }
+
+    #// Function to change the image in the UI to be empty    
+function StatusUnBang {
+        $image.imageLocation = $Assets.ImgEmpty
+    }
+
+    #// Function to start a job and run a Wscript to press the scroll-lock key every 45sec which keeps the computer awake
+function StayAwake {
+        StatusBang
+        Start-Job -Name Wakey {
+        Write-Host functionRan
+            $WShell = New-Object -Com Wscript.Shell
+            while (1) {$WShell.SendKeys("{SCROLLLOCK}{SCROLLLOCK}"); Start-Sleep 45}
+            Write-Host functionRan2
+        }
+    }
+    
+    #// Function to kill the Wscript process and stop the job
+function KillAwake {
+        Try {
+            Stop-Job -Name Wakey
+            Remove-Job -Name Wakey
+            StatusUnBang
+            }
+        Catch {
+            Write-Host No Job is currently running.
+        }
+        Finally {
+        }
+    }
+#endregion /\ FUNCTIONS /\
+   
+
+#region \/ GUI \/
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = New-Object System.Drawing.Point(161,207)
+$Form.text                       = "Form"
+$Form.TopMost                    = $false
+$Form.Icon                       = $Assets.Icon
+
+$image                           = New-Object system.Windows.Forms.PictureBox
+$image.width                     = 147
+$image.height                    = 121
+$image.location                  = New-Object System.Drawing.Point(5,5)
+$image.imageLocation             = ""
+$image.SizeMode                  = [System.Windows.Forms.PictureBoxSizeMode]::zoom
+$buttonBang                      = New-Object system.Windows.Forms.Button
+$buttonBang.text                 = "Coffee!"
+$buttonBang.width                = 144
+$buttonBang.height               = 30
+$buttonBang.location             = New-Object System.Drawing.Point(8,128)
+$buttonBang.Font                 = New-Object System.Drawing.Font('Consolas',17,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+$buttonBang.BackColor            = [System.Drawing.ColorTranslator]::FromHtml("#7ed321")
+
+$buttonUnbang                    = New-Object system.Windows.Forms.Button
+$buttonUnbang.text               = "UN-Coffee!"
+$buttonUnbang.width              = 144
+$buttonUnbang.height             = 30
+$buttonUnbang.location           = New-Object System.Drawing.Point(8,166)
+$buttonUnbang.Font               = New-Object System.Drawing.Font('Consolas',17,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+$buttonUnbang.BackColor          = [System.Drawing.ColorTranslator]::FromHtml("#d0021b")
+
+$Form.controls.AddRange(@($image,$buttonBang,$buttonUnbang))
+
+    #// Button to start job
+$buttonBang.Add_Click({ StayAwake })
+    #// Button to stop job
+$buttonUnbang.Add_Click({ KillAwake })
+#endregion /\ GUI /\
+
+    #// Run form
+[void]$Form.ShowDialog()
+    
